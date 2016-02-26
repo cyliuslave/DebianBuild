19b32da218996848ce3c1ae656f62256880f6b46
cyliu <cyliu@debian>
2016-02-26 16:34:01 +0800

    cool

--- prerequisite.sh
+++ prerequisite.sh
@@ -1,1 +1,1 @@
-sudo apt-get install sudo vim build-essential libtool autoconf libglib2.0-dev zlib1g-dev
+sudo apt-get install sudo vim build-essential libtool autoconf libglib2.0-dev zlib1g-dev git install libsdl2-dev
