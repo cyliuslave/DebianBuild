10711e3fc0c18c655a7d9fc6bf31893c74a81a8c
cyliustack <cyliustack@gmail.com>
2016-02-26 16:40:55 +0800

    cool

--- prerequisite.sh
+++ prerequisite.sh
@@ -1,1 +1,5 @@
 sudo apt-get install sudo vim build-essential libtool autoconf libglib2.0-dev zlib1g-dev git install libsdl2-dev
+git clone https://github.com/json-c/json-c.git
+cd json-c  
+sh autogen.sh
+
